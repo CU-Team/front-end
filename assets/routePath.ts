c5da2c1783bcf799e0af406b2eaa74a41f15a9d3
jeongjiwoo-include <jjw89506343@gmail.com>
2022-09-03 15:06:19 +0900

    [develop] add routePath at asset

---
+++ assets/routePath.ts
@@ -0,0 +1,5 @@
+export const routePath = {
+  index: '/',
+  login: '/login',
+  my: 'my',
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import { routePath } from '@assets/routePath';
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(routePath.login);
+  };
+
   return <StyledWrapper>home</StyledWrapper>;
 };
 
